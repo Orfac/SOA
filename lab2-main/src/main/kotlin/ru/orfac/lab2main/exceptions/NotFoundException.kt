1a44e2d9c593791fe987882089898d9336425fa1
Arseniy Molodetskiy <molodeckij.b.2017@gmail.com>
2020-12-26 00:18:39 +0300

    delete useless project

--- lab2-main/src/main/kotlin/ru/orfac/lab2main/exceptions/NotFoundException.kt
+++
@@ -1,7 +0,0 @@
-package ru.orfac.lab2main.exceptions
-
-import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.ResponseStatus
-import java.lang.Exception
-
-class NotFoundException(msg: String) : RuntimeException(msg)
--- lab2-main/src/main/kotlin/ru/orfac/lab2main/exceptions/RequestHandlingException.kt
+++
@@ -1,3 +0,0 @@
-package ru.orfac.lab2main.exceptions
-
-class RequestHandlingException(msg: String) : RuntimeException(msg)
